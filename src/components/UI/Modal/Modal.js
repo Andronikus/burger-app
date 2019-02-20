6f7239a5c0e25679382cbc2bcc27c9c650da64d5
Andronikus <tiagoafroque@gmail.com>
2019-02-20 09:56:34 +0000

    cleaned modal component

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,26 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './Modal.css';
 import Auxiliary from '../../../hoc/Auxiliary';
 import BackDrop from '../Backdrop/Backdrop';
 
+class Modal extends Component {
 
+    shouldComponentUpdate(nextProps){
+        return nextProps.show !== this.props.show;
+    }
 
-const modal = (props) => (
-    
-    <Auxiliary>
-        <BackDrop show={props.show} clicked={props.modalClosed}></BackDrop>
-        <div 
-            className={classes.Modal}
-            style={
-                {
-                    transform: props.show ? 'translateY(0)': 'translateY(-100vh)',
-                    opacity: props.show ? '1' : '0'
-                }
-            }
-        >
-            {props.children}
-        </div>
-    </Auxiliary>
-);
+    render(){
 
-export default modal;
+        return(
+            <Auxiliary>
+                <BackDrop show={this.props.show} clicked={this.props.modalClosed}></BackDrop>
+                <div 
+                    className={classes.Modal}
+                    style={
+                        {
+                            transform: this.props.show ? 'translateY(0)': 'translateY(-100vh)',
+                            opacity: this.props.show ? '1' : '0'
+                        }
+                    }
+                >
+                    {this.props.children}
+                </div>
+            </Auxiliary>
+        );
+    }
+}
+
+export default Modal;
