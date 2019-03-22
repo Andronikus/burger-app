fa9672b85d4385bc74554edbefccfbcf0f23575c
Tiago Roque <andronikus@MBP-de-Tiago.lan>
2019-03-22 23:17:16 +0000

    added contact data module

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
+import ContactData from '../Checkout/ContactData/ContactData';
 
 class Checkout extends Component{
 
@@ -40,6 +42,7 @@ class Checkout extends Component{
                     continueCheckout={this.continueCheckoutHandler} 
                     ingredients={this.state.ingredients} 
                 />
+                <Route path={this.props.match.path + '/contact-data'} component={ContactData}/>
             </div>
         );
     }
