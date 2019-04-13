bf1e5e16ae159e61f585d22d38bc4b843621719c
Tiago Roque <andronikus@MBP-de-Tiago.lan>
2019-04-13 23:17:17 +0100

    organized store folder with action and reducer folders

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -9,7 +9,7 @@ import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import axios from '../../axios-orders';
 import Spinner from '../../components/UI/Spinner/Spinner';
 import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
-import * as actiosTypes from '../../store/actions';
+import * as actiosTypes from '../../store/actions/actionTypes';
 
 class BurgerBuilder extends Component {
 
