2ca768166691e8bcdf04ed94e15f822723f7b465
andronikus <tiagoafroque@gmail.com>
2019-04-25 22:35:15 +0100

    adding guards to routes only accessible when auth

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import Layout from './hoc/Layout/Layout';
@@ -11,33 +11,55 @@ import Logout from './containers/Auth/Logout/Logout';
 import * as actions from './store/actions/index';
 
 class App extends Component {
-  
-  componentDidMount(){
+
+  componentDidMount() {
     this.props.onTryAutoSignin();
   }
   
-  render() {
-    return (
-      <div>
-        <Layout>
-          <Switch>
-            <Route path="/checkout" component={Checkout}/>
-            <Route path="/orders" component={Orders}/>
-            <Route path="/auth" component={Auth}/>
-            <Route path="/logout" component={Logout}/>
-            <Route path="/" component={BurgerBuilder}/>
-          </Switch>
-        </Layout>
-      </div>
+ 
+
+render() {
+  let routers = (
+    <Switch>
+      <Route path="/auth" component={Auth} />
+      <Route path="/" exact component={BurgerBuilder} />
+      <Redirect to="/"/>
+    </Switch>
+  );
+
+  if(this.props.isAuthenticated){
+    routers = (
+      <Switch>
+          <Route path="/checkout" component={Checkout} />
+          <Route path="/orders" component={Orders} />
+          <Route path="/logout" component={Logout} />
+          <Route path="/" component={BurgerBuilder} />
+          <Redirect to="/"/>
+      </Switch>
     );
+  }
+
+  return (
+    <div>
+      <Layout>
+        {routers}
+      </Layout>
+    </div>
+  );
+}
+}
+
+const mapStateToProps = state => {
+  return {
+    isAuthenticated: state.auth.token !== null
   }
 }
 
 const mapDispatchToProps = dispatch => {
-  return{
+  return {
     onTryAutoSignin: () => (dispatch(actions.authCheckState()))
   }
-  
+
 }
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
