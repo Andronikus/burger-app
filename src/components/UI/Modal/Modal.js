02d2ee05af59d23236f805de78f1e3b43892c027
andronikus <tiagoafroque@gmail.com>
2019-06-15 21:51:26 +0100

    transform BurgerIngredients and Modal  class to functional components

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,33 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
 import classes from './Modal.css';
 import Auxiliary from '../../../hoc/Auxiliary/Auxiliary';
 import BackDrop from '../Backdrop/Backdrop';
 
-class Modal extends Component {
-
-    shouldComponentUpdate(nextProps, nextState){
-        return nextProps.show !== this.props.show || nextProps.children !== this.props.children;
-    }
-
-    render(){
-
-        return(
-            <Auxiliary>
-                <BackDrop show={this.props.show} clicked={this.props.modalClosed}></BackDrop>
-                <div 
-                    className={classes.Modal}
-                    style={
-                        {
-                            transform: this.props.show ? 'translateY(0)': 'translateY(-100vh)',
-                            opacity: this.props.show ? '1' : '0'
-                        }
+const modal = (props) => {
+    return (
+        <Auxiliary>
+            <BackDrop show={props.show} clicked={props.modalClosed}></BackDrop>
+            <div
+                className={classes.Modal}
+                style={
+                    {
+                        transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                        opacity: props.show ? '1' : '0'
                     }
-                >
-                    {this.props.children}
-                </div>
-            </Auxiliary>
-        );
-    }
+                }
+            >
+                {props.children}
+            </div>
+        </Auxiliary>
+    );
 }
 
-export default Modal;
+const areEqual = (prevProps, nextProps) => {return nextProps.show === prevProps.show && nextProps.children === prevProps.children}
+
+export default React.memo(modal, areEqual);
