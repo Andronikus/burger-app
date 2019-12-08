e410fc5e4a158c671468081050b31b5c76b8f59f
Andronikus <tiagoafroque@gmail.com>
2019-12-08 19:53:03 +0000

    fix the composeEnhancement for dev running

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ const rootReducer = combineReducers({
 
 const sagaMiddleware = createSagaMiddleware();
 
-const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
+const composeEnhancers = (process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null) || compose;
 const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk, sagaMiddleware)));
 
 sagaMiddleware.run(watchAuth);
